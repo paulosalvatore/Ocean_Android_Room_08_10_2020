b5f810abdd82e2fe92ade15ac051f8d3ac905cdf
Paulo Salvatore <paulo@salvatore.tech>
2020-10-08 20:36:06 -0300

    Imports do WordListAdapter adicionados

--- app/src/main/java/com/oceanbrasil/ocean_android_room_08_10_2020/view/WordListAdapter.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_room_08_10_2020/view/WordListAdapter.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.oceanbrasil.ocean_android_room_08_10_2020.R
+import com.oceanbrasil.ocean_android_room_08_10_2020.model.Word
 
 class WordListAdapter(context: Context) :
     RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
