ae4e37958b5d8c4b443a96bcccb1c480e2f096bf
Paulo Salvatore <paulo@salvatore.tech>
2020-10-08 20:17:02 -0300

    Implementação do RoomDatabase callback para popular banco ao criar

--- app/src/main/java/com/oceanbrasil/ocean_android_room_08_10_2020/model/WordRoomDatabase.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_room_08_10_2020/model/WordRoomDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 @Database(entities = [Word::class], version = 1, exportSchema = false)
 abstract class WordRoomDatabase : RoomDatabase() {
@@ -11,6 +12,26 @@ abstract class WordRoomDatabase : RoomDatabase() {
     abstract fun wordDao(): WordDao
 
     companion object {
+        private val roomDatabaseCallback = object : Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+
+                instance?.let {
+                    Thread(Runnable {
+                        val dao = it.wordDao()
+
+                        dao.deleteAll()
+
+                        val word = Word("Samsung")
+                        dao.insert(word)
+
+                        val word2 = Word("Ocean")
+                        dao.insert(word2)
+                    }).start()
+                }
+            }
+        }
+
         private var instance: WordRoomDatabase? = null
 
         fun getDatabase(context: Context): WordRoomDatabase {
@@ -21,7 +42,7 @@ abstract class WordRoomDatabase : RoomDatabase() {
                         context.applicationContext,
                         WordRoomDatabase::class.java,
                         "word_database"
-                    ).build()
+                    ).addCallback(roomDatabaseCallback).build()
                 }
             }
 
