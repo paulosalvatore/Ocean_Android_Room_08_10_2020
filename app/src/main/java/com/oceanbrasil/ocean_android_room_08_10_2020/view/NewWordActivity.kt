089de92d88bca2e2586e1992c917bea6cdfac1a3
Paulo Salvatore <paulo@salvatore.tech>
2020-10-08 21:09:45 -0300

    Funcionamento da new word activity criado

--- app/src/main/java/com/oceanbrasil/ocean_android_room_08_10_2020/view/NewWordActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_room_08_10_2020/view/NewWordActivity.kt
@@ -1,12 +1,31 @@
 package com.oceanbrasil.ocean_android_room_08_10_2020.view
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.oceanbrasil.ocean_android_room_08_10_2020.R
+import kotlinx.android.synthetic.main.activity_new_word.*
 
 class NewWordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_word)
+
+        btSave.setOnClickListener {
+            val replyIntent = Intent()
+
+            if (etWord.text.isBlank()) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val word = etWord.text.toString()
+
+                replyIntent.putExtra("REPLY_WORD", word)
+
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+
+            finish()
+        }
     }
 }
