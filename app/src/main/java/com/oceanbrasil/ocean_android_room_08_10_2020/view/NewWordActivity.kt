7097b9d32ed4d23fb311c29e1f0ecd53014c896b
Paulo Salvatore <paulo@salvatore.tech>
2020-10-08 21:03:58 -0300

    New word activity criada

---
+++ app/src/main/java/com/oceanbrasil/ocean_android_room_08_10_2020/view/NewWordActivity.kt
@@ -0,0 +1,12 @@
+package com.oceanbrasil.ocean_android_room_08_10_2020.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.oceanbrasil.ocean_android_room_08_10_2020.R
+
+class NewWordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+    }
+}
